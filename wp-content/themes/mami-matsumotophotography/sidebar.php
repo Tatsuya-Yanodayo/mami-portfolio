c13b1cabc5e2d20f871fa1aca4a835606ad7194a
Tatsuya Yano <tatsuyanodayo@gmail.com>
2018-04-30 17:43:25 +0900

    [add]ポートフォリオページのタグ追加

--- wp-content/themes/mami-matsumotophotography/sidebar.php
+++ wp-content/themes/mami-matsumotophotography/sidebar.php
@@ -6,6 +6,8 @@
 
 	<ul id="filters" class="filter-group">
 		<li><a href="#" class="link-filter active" data-filter=".Ray">RAY</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Dark">●</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Light">◯</a></li>
 		<li><a href="#" class="link-filter" data-filter=".Japan">JAPAN</a></li>
 		<li><a href="#" class="link-filter" data-filter=".World">WORLD</a></li>
 		<li><a href="#" class="link-filter" data-filter=".Bike">BIKE</a></li>
