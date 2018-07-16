b58ee5ceadc403f6e41482c126011f1dbbbfe5cb
Tatsuya Yano <tatsuyanodayo@gmail.com>
2018-07-16 18:19:40 +0900

    [add]写真カテゴリー追加

--- wp-content/themes/mami-matsumotophotography/sidebar.php
+++ wp-content/themes/mami-matsumotophotography/sidebar.php
@@ -5,14 +5,16 @@
 <div id="category">
 
 	<ul id="filters" class="filter-group">
-		<li><a href="#" class="link-filter active" data-filter=".Ray">RAY</a></li>
+		<li><a href="#" class="link-filter active" data-filter=".Ray">Ray</a></li>
 		<li><a href="#" class="link-filter" data-filter=".Dark">●</a></li>
 		<li><a href="#" class="link-filter" data-filter=".Light">◯</a></li>
-		<li><a href="#" class="link-filter" data-filter=".Japan">JAPAN</a></li>
-		<li><a href="#" class="link-filter" data-filter=".World">WORLD</a></li>
-		<li><a href="#" class="link-filter" data-filter=".Bike">BIKE</a></li>
-		<li><a href="#" class="link-filter" data-filter=".Fashion">FASHION</a></li>
-		<li><a href="#" class="link-filter" data-filter=".Artists">ARTISTS</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Portrait">Portrait</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Landscape">Landscape</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Film">Film</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Bike">Bike</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Fashion">Fashion</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Artists">Artists</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Lovegraph">Lovegraph</a></li>
 	</ul>
 	
 </div>
