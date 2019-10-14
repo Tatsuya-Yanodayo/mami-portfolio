6215a4c1e6a572dd1d71f6babf4a5333f4a20066
matsumoto-mami <mame4630@gmail.com>
2019-10-14 12:43:49 +0900

    add category

--- wp-content/themes/mami-matsumotophotography/sidebar.php
+++ wp-content/themes/mami-matsumotophotography/sidebar.php
@@ -6,14 +6,20 @@
 
 	<ul id="filters" class="filter-group">
 		<li><a href="#" class="link-filter active" data-filter=".Ray">Ray</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Youth">Bloom of youth</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Dressed">Dressed</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Tibet">Tibet</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Japan">Japan</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Urban">Urban</a></li>
 		<li><a href="#" class="link-filter" data-filter=".Dark">●</a></li>
-		<li><a href="#" class="link-filter" data-filter=".Light">◯</a></li>
-		<li><a href="#" class="link-filter" data-filter=".Portrait">Portrait</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Spring">Spring</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Summer">Summer</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Autumn">Autumn</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Winter">Winter</a></li>
 		<li><a href="#" class="link-filter" data-filter=".Landscape">Landscape</a></li>
-		<li><a href="#" class="link-filter" data-filter=".Film">Film</a></li>
 		<li><a href="#" class="link-filter" data-filter=".Bike">Bike</a></li>
 		<li><a href="#" class="link-filter" data-filter=".Fashion">Fashion</a></li>
-		<li><a href="#" class="link-filter" data-filter=".Artists">Artists</a></li>
+		<li><a href="#" class="link-filter" data-filter=".Proposal">Proposal</a></li>
 		<li><a href="#" class="link-filter" data-filter=".Lovegraph">Lovegraph</a></li>
 	</ul>
 	
